05b0eda017d4fd22047a120c714e6780fad626b7
vict <ychtan@email.gwu.edu>
2016-05-26 17:03:55 -0400

    adding weibo and try to separate twitter and weibo

--- sfm_elk_loader.py
+++ sfm_elk_loader.py
@@ -23,19 +23,24 @@ class ElkLoader(BaseConsumer):
             return
 
         harvest_type = self.message["harvest"]["type"]
+        jq_cmd = "jq -c '{ sm_type: \"tweet\", id: .id, user_id: .user.id_str, " \
+                 "screen_name: .user.screen_name, created_at: .created_at, text: .text, " \
+                 "user_mentions: [.entities.user_mentions[]?.screen_name], " \
+                 "hashtags: [.entities.hashtags[]?.text], " \
+                 "urls: [.entities.urls[]?.expanded_url]}'"
         if harvest_type in ('twitter_search', 'twitter_user_timeline'):
-            iter_type = "rest"
+            iter_type = "twitter_rest_warc_iter.py"
         elif harvest_type in ('twitter_sample', 'twitter_filter'):
-            iter_type = "stream"
+            iter_type = "twitter_stream_warc_iter.py"
+        elif harvest_type == 'weibo_timeline':
+            iter_type = "weibo_warc_iter.py"
+            jq_cmd = "jq -c '{ sm_type: \"weibo\", id: .mid, user_id: .user.idstr, " \
+                     "screen_name: .user.screen_name, created_at: .created_at, text: .text}'"
         else:
             log.info("Skipping %s since do not handle %s", warc_filepath, harvest_type)
             return
         log.info("Loading %s", warc_filepath)
-        cmd = "twitter_{}_warc_iter.py {} | jq -c '{{ id: .id, user_id: .user.id_str, " \
-              "screen_name: .user.screen_name, created_at: .created_at, text: .text, " \
-              "user_mentions: [.entities.user_mentions[]?.screen_name], hashtags: [.entities.hashtags[]?.text], " \
-              "urls: [.entities.urls[]?.expanded_url]}}' | /opt/logstash/bin/logstash -f stdin.conf".format(
-                iter_type, warc_filepath)
+        cmd = "{} {} | {} | /opt/logstash/bin/logstash -f stdin.conf".format(iter_type, warc_filepath, jq_cmd)
 
         try:
             check_output(cmd, shell=True)
@@ -43,30 +48,31 @@ class ElkLoader(BaseConsumer):
         except CalledProcessError, e:
             log.error("%s returned %s: %s", cmd, e.returncode, e.output)
 
+
 if __name__ == "__main__":
-        # Logging
-        logging.basicConfig(format='%(asctime)s: %(name)s --> %(message)s', level=logging.DEBUG)
+    # Logging
+    logging.basicConfig(format='%(asctime)s: %(name)s --> %(message)s', level=logging.DEBUG)
 
-        # Arguments
-        parser = argparse.ArgumentParser()
-        parser.add_argument("host")
-        parser.add_argument("username")
-        parser.add_argument("password")
-        parser.add_argument("queue")
-        parser.add_argument("--collection-set", help="Limit to load to collection set with this collection set id.")
-        parser.add_argument("--debug", type=lambda v: v.lower() in ("yes", "true", "t", "1"), nargs="?",
-                            default="False", const="True")
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host")
+    parser.add_argument("username")
+    parser.add_argument("password")
+    parser.add_argument("queue")
+    parser.add_argument("--collection-set", help="Limit to load to collection set with this collection set id.")
+    parser.add_argument("--debug", type=lambda v: v.lower() in ("yes", "true", "t", "1"), nargs="?",
+                        default="False", const="True")
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
-        # Logging
-        logging.basicConfig(format='%(asctime)s: %(name)s --> %(message)s',
-                            level=logging.DEBUG if args.debug else logging.INFO)
+    # Logging
+    logging.basicConfig(format='%(asctime)s: %(name)s --> %(message)s',
+                        level=logging.DEBUG if args.debug else logging.INFO)
 
-        # Adding a queue name that is prefixed with this host. This will allow sending messages directly
-        # to this queue. This approach could be generalized so that the queue specific binding is created
-        # and the queue name is automatically removed.
-        loader = ElkLoader(collection_set_id=args.collection_set,
-                           mq_config=MqConfig(args.host, args.username, args.password, EXCHANGE,
-                                              {args.queue: ["warc_created", "{}.warc_created".format(args.queue)]}))
-        loader.run()
+    # Adding a queue name that is prefixed with this host. This will allow sending messages directly
+    # to this queue. This approach could be generalized so that the queue specific binding is created
+    # and the queue name is automatically removed.
+    loader = ElkLoader(collection_set_id=args.collection_set,
+                       mq_config=MqConfig(args.host, args.username, args.password, EXCHANGE,
+                                          {args.queue: ["warc_created", "{}.warc_created".format(args.queue)]}))
+    loader.run()
