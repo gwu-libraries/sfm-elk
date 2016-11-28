36fd3b897986165e487d3c07b34bf19fb8708b7e
Justin Littman <justinlittman@gwu.edu>
2016-11-28 13:34:44 -0500

    refs sfm 532. Sets sfm user and group.

--- docker/start.sh
+++ docker/start.sh
@@ -14,14 +14,14 @@ echo path.data: /sfm-data/elk/$HOSTNAME/data >> /etc/elasticsearch/elasticsearch
 /usr/local/bin/start.sh &
 
 echo "Waiting for elk"
-appdeps.py --port-wait mq:5672 --port-wait localhost:9200 --port-wait localhost:5601 --wait-secs 60
+appdeps.py --port-wait mq:5672 --port-wait localhost:9200 --port-wait localhost:5601 --wait-secs 90 --file-wait /sfm-data/collection_set --file-wait /sfm-data/containers
 if [ "$?" = "1" ]; then
     echo "Problem with application dependencies."
     exit 1
 fi
 
-python elk_config_loader.py
+gosu sfm python elk_config_loader.py
 
-python sfm_elk_loader.py mq $RABBITMQ_USER $RABBITMQ_PASSWORD elk_loader_$HOSTNAME --debug=$DEBUG $* &
+gosu sfm python sfm_elk_loader.py mq $RABBITMQ_USER $RABBITMQ_PASSWORD elk_loader_$HOSTNAME --debug=$DEBUG $* &
 
 wait
